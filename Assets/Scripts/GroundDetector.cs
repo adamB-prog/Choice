7ea1e67c22dcc2ddbd86f9f2ab77b87bf0d60fa0
Básthy Ádám László <basthyadi@gmail.com>
2022-06-30 18:17:06 +0200

    First project files

---
+++ Assets/Scripts/GroundDetector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal class GroundDetector : MonoBehaviour, IGroundDetection
+    {
+        [SerializeField]
+        private string groundTag;
+
+        
+        public bool OnGround { get; private set; }
+
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag.Equals(groundTag))
+            {
+                OnGround = true;
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            OnGround = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,114 @@
+
+using Assets.Scripts;
+using Choice.Input;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private PlayerControls playerControls;
+    
+    private Rigidbody2D rb;
+
+
+    private IGroundDetection groundDetector;
+
+    private bool moving = false;
+
+    
+    private bool jumping = false;
+
+    
+    
+
+    [SerializeField]
+    private float speed = 1f;
+
+    [SerializeField]
+    private float jumpForce = 1f;
+
+    [SerializeField]
+    private float maxVelocity = 1f;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        groundDetector = gameObject.GetComponentInChildren<IGroundDetection>();
+        
+
+        playerControls.Land.Jump.performed += Jump;
+        playerControls.Land.Jump.canceled += Jump_canceled;
+        playerControls.Land.Move.performed += Move;
+        playerControls.Land.Move.canceled += Move_Canceled;
+    }
+
+    private void Jump_canceled(InputAction.CallbackContext obj)
+    {
+        jumping = false;
+    }
+
+    private void Move_Canceled(InputAction.CallbackContext obj)
+    {
+        moving = false;
+    }
+
+    private void Move(InputAction.CallbackContext context)
+    {
+        Debug.Log(context.ReadValue<float>());
+        moving = true;
+        
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (moving)
+        {
+            float movementX = playerControls.Land.Move.ReadValue<float>();
+            rb.AddForce(movementX * transform.right * speed);
+        }
+        
+        if (jumping && groundDetector.OnGround)
+        {
+            rb.AddForce(transform.up * jumpForce);
+
+           
+        }
+
+        rb.velocity = ClampOnX(rb.velocity, maxVelocity);
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jump");
+        
+
+        jumping = true;
+    }
+
+    
+
+
+    private Vector2 ClampOnX(Vector2 vector, float maxX)
+    {
+        return new Vector2(Math.Clamp(vector.x, -maxX, maxX), vector.y);
+    }
+}
