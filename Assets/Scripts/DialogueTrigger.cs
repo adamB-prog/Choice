58cd930d109884dc02996aaac8600cdb5751449b
Básthy Ádám László <basthyadi@gmail.com>
2022-07-12 17:03:43 +0200

    DialogueTrigger finished

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -9,18 +9,35 @@ public class DialogueTrigger : MonoBehaviour
     [SerializeField]
     private GameObject visualCue;
 
+    [Header("Ink JSON")]
+    [SerializeField]
+    private TextAsset inkJSON;
+
+
+
+
     private IInRangeDetection inRangeDetection;
+
+    private void Awake()
+    {
+        inRangeDetection = GetComponent<IInRangeDetection>();
+    }
 
     private void FixedUpdate()
     {
-        if (inRangeDetection.IsInRange)
+        if (inRangeDetection.IsInRange && !DialogueManager.GetInstance().DialogueIsPlaying)
         {
             visualCue.SetActive(true);
-            
+            if (InputManager.GetInstance().GetInteractPressed())
+            {
+                DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+            }
         }
         else
         {
             visualCue.SetActive(false);
         }
     }
+
+
 }
