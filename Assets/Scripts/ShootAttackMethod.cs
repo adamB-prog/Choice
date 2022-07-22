2bb1b167b1163b0bcdd8f0b38b8fe31d4854f6a6
Básthy Ádám László <basthyadi@gmail.com>
2022-07-22 12:50:32 +0200

    Create ShootAttackMethod class

---
+++ Assets/Scripts/ShootAttackMethod.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Scripts.Interfaces;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal class ShootAttackMethod : MonoBehaviour, IAttackMethod
+    {
+        //private float timer;
+
+        private bool canAttack = true;
+
+        [SerializeField]
+        private float attackTimer = 1f;
+
+        [SerializeField]
+        private GameObject bullet;
+
+
+        public void Attack()
+        {
+            if (canAttack)
+            {
+                PlaceProjectile();
+
+                ReloadTime();
+            }
+        }
+
+
+        private IEnumerator ReloadTime()
+        {
+            canAttack = false;
+            yield return new WaitForSeconds(attackTimer);
+            canAttack = true;
+
+        }
+
+        private void PlaceProjectile()
+        {
+            Instantiate(bullet);
+        }
+    }
+}
