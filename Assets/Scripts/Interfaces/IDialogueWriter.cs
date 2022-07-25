a11fdebcf38e64be7aa467fd0192fe0d6cab51d6
Básthy Ádám László <basthyadi@gmail.com>
2022-07-25 13:02:32 +0200

    Add Property to interface

--- Assets/Scripts/Interfaces/IDialogueWriter.cs
+++ Assets/Scripts/Interfaces/IDialogueWriter.cs
@@ -4,6 +4,7 @@ namespace Assets.Scripts.Interfaces
 {
     internal interface IDialogueWriter
     {
+        public bool IsFinished { get; }
         void WriteDialogueText(TextMeshProUGUI tmpro, string text);
     }
 }
